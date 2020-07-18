30dc0cdd22e24cb7f6240b30b8bfe1bbd99c220f
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-18 16:21:08 -0700

    Declare Variables

--- Distance-App/Distance-App/ViewController.m
+++ Distance-App/Distance-App/ViewController.m
@@ -7,17 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "DistanceGetter/DGDistanceRequest.h"
 
 @interface ViewController ()
+@property (nonatomic) DGDistanceRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *startLoaction;
+@property (weak, nonatomic) IBOutlet UITextField *endLocationA;
+@property (weak, nonatomic) IBOutlet UILabel *distanceA;
+@property (weak, nonatomic) IBOutlet UITextField *endLocationB;
+@property (weak, nonatomic) IBOutlet UILabel *distanceB;
+@property (weak, nonatomic) IBOutlet UILabel *distanceC;
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
+@property (weak, nonatomic) IBOutlet UITextField *endLocationC;
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (IBAction)calculateButtonTapped:(id)sender {
+    
+  
 }
-
 
 @end
